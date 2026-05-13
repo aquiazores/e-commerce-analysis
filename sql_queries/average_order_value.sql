70271aa329f9632e01d3d127dec8625891906530
aquiazores <azoresaquila@gmail.com>
2026-05-13 20:24:34 +0800

    Create average_order_value.sql

---
+++ sql_queries/average_order_value.sql
@@ -0,0 +1,8 @@
+SELECT
+  SUM(payment_value) / COUNT(DISTINCT o.order_id) as AOV,
+  FORMAT_TIMESTAMP('%Y-%m', o.order_purchase_timestamp) as Month
+FROM `olist.clean_payments` p
+JOIN `olist.clean_orders` o ON p.order_id = o.order_id
+WHERE o.order_status = 'delivered'
+GROUP BY Month
+ORDER BY Month ASC
